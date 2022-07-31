b67b41d218339daf0d9e6c076265a3843a7688f8
Clayman <Clayman@DESKTOP-DM8E77U>
2022-07-31 15:02:21 +0300

    Arrays day 1

---
+++ Arrays/Arrays/main.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int n = 5;
+	int arr[n] = { 3,5,8 };
+	//arr[2] = 123;//Обращение к элементу массива на запись
+	//cout << arr[2] << endl;	//Обращение к тому же элементу, на чтение
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+}

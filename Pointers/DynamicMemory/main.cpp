aef5462792863f75707c795e36fcea2377c3016e
Clayman <Clayman@DESKTOP-DM8E77U>
2022-11-06 10:57:34 +0200

    Dynamic memory day 2

--- Pointers/DynamicMemory/main.cpp
+++ Pointers/DynamicMemory/main.cpp
@@ -4,6 +4,8 @@ using namespace std;
 void FillRand(int arr[], const int n);
 void Print(int arr[], const int n);
 
+int* push_back(int arr[], int& n, int value);
+
 void main()
 {
 	setlocale(LC_ALL, "");
@@ -13,6 +15,19 @@ void main()
 	
 	FillRand(arr, n);
 	Print(arr, n);
+
+	int value;
+	cout << "Введите добавляемое значение: "; cin >> value;
+	arr = push_back(arr, n, value);
+
+	Print(arr, n);
+
+	/*
+					DEBUG ASSERTION FAILED
+		1. Удаляется область стека, т.е., когда оператору delete[] был передан адрес переменной или массива,
+		   объявленного в функции main() или любой другой функции, т.е., статический массив;
+		2. Оператору delete[] повторно передается один и тот же адрес;
+	*/
 	
 	delete[] arr;
 }
@@ -31,4 +46,21 @@ void Print(int arr[], const int n)
 		cout << arr[i] << "\t";
 	}
 	cout << endl;
+}
+
+int* push_back(int arr[], int& n, int value)
+{
+	//1) Создаем буферный массив нужного размера:
+	int* buffer = new int[n + 1]{};
+	//2) Копируем все значения из исходного массива в буферный:
+	for (int i = 0; i < n; i++)buffer[i] = arr[i];
+	//3) Удаляем исходный массив:
+	delete[] arr;
+	//4) Подменяем адрес исходного массива адресом буфера:
+	arr = buffer;
+	//5) Только теперь в массив arr можно добавить еще одно значение, потому что в массиве появился еще один элемент:
+	arr[n] = value;
+	//6) После добавления элемента в массив, количество его элементов увеличивается на 1:
+	n++;
+	return arr;
 }
---
+++ Pointers/Split/main.cpp
@@ -0,0 +1,53 @@
+﻿#include<iostream>
+using namespace std;
+
+void FillRand(int arr[], const int n);
+void Print(int arr[], const int n);
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int n = 10;
+	int arr[n];
+	FillRand(arr, n);
+	Print(arr, n);
+
+	int even_count = 0;	//количество четных элементов
+	int odd_count = 0;	//количество нечетных элементов
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] % 2 == 0)even_count++;
+		else odd_count++;
+	}
+
+	int* even_arr = new int[even_count] {};
+	int* odd_arr = new int[odd_count] {};
+
+	for (int i = 0, j = 0, k = 0; i < n; i++)
+	{
+		if (arr[i] % 2 == 0)even_arr[j++] = arr[i];
+		else odd_arr[k++] = arr[i];
+	}
+
+	Print(even_arr, even_count);
+	Print(odd_arr, odd_count);
+
+	delete[] odd_arr;
+	delete[] even_arr;
+}
+
+void FillRand(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % 100;
+	}
+}
+void Print(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] <<"\t";
+	}
+	cout << endl;
+}
